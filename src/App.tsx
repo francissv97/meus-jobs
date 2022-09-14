c246feed7dfb5c70cec967641ca2250ca4a64e01
Francis Verissimo <francissv97@gmail.com>
2022-09-14 19:33:47 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+export function App() {
+  return (
+    <div className="bg-zinc-600 p-4">
+      <h1 className="text-5xl text-zinc-200">
+        {"Meus"}
+        <span className="text-orange-500">{"JOBS"}</span>
+      </h1>
+    </div>
+  );
+}
