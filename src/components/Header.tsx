afa3fc1c10fc12eae8128a42d1ffad9f703c6264
Francis Verissimo <francissv97@gmail.com>
2022-09-29 20:12:03 -0400

    create new components

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import { Logo } from "./Logo";
+
+export function Header() {
+  return (
+    <div className="bg-zinc-600 flex items-center justify-center pt-4 pb-16">
+      <Logo />
+    </div>
+  );
+}
---
+++ src/components/JobCard.tsx
@@ -0,0 +1,47 @@
+import { Button } from "antd";
+import { PencilSimpleLine, Trash } from "phosphor-react";
+
+export function JobCard() {
+  return (
+    <div className="grid grid-cols-3 bg-zinc-100 gap-4 px-4 py-6 rounded mx-4">
+      <div className="flex col-span-2 flex-col gap-2">
+        <div id="JobName" className="flex items-center">
+          <span className="text-zinc-700 text-xl font-medium overflow-hidden text-ellipsis">JobWorkFrellaNamessssssssssss</span>
+        </div>
+
+        <div id="JobPrazo" className="flex flex-col">
+          <span className="text-sm text-zinc-500">Prazo</span>
+          <span className="text-zinc-700">ValueValue</span>
+        </div>
+
+        <div id="JobValor" className="flex flex-col justify-center">
+          <span className="text-sm text-zinc-500">Valor</span>
+          <span className="text-zinc-700">R$ 000.000,00</span>
+        </div>
+      </div>
+
+      <div className="flex flex-col justify-between">
+        <div
+          id="JobStatus"
+          className="bg-red-300/20 text-red-700 px-2 py-1 text-sm text-center rounded w-fit self-end"
+        >
+          <span className="whitespace-nowrap">Em andamento</span>
+        </div>
+
+        <div id="JobHomeComponentButtons" className="flex justify-end gap-2">
+          <Button
+            id="JobEdit"
+            icon={<PencilSimpleLine size={22} />}
+            className="flex items-center justify-center bg-zinc-500 text-white p-2 rounded transition hover:bg-zinc-600"
+          />
+          <Button
+            id="JobRemove"
+            shape="circle"
+            icon={<Trash size={22} />}
+            className="flex items-center justify-center bg-red-700 text-white p-2 rounded transition hover:bg-red-800"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
