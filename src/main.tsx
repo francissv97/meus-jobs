5fcc42ef1b3e4b27a19d3eb8220c92deae88995d
Francis Verissimo <francissv97@gmail.com>
2022-10-04 19:27:18 -0400

    import AntDesign CSS

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "./services/firebase";
+import "antd/dist/antd.css";
 import "./styles/main.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
