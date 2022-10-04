889a21f02a1330b6b645cc7a49776a249592e264
Francis Verissimo <francissv97@gmail.com>
2022-10-04 11:25:58 -0400

    add UserInfo Popover

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -1,18 +1,50 @@
+import { Button, Popover } from "antd";
+import {
+  CaretDownOutlined,
+  LogoutOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import "antd/dist/antd.css";
+
 interface Props {
   name: string;
   avatar: string;
 }
 
+const popoverContent = (
+  <div className="m-0 flex flex-col gap-2">
+    <Button
+      className="flex items-center justify-center text-base"
+      icon={<UserOutlined className="text-lg" />}
+    >
+      Perfil
+    </Button>
+    <Button
+      danger
+      className="flex items-center justify-center text-base"
+      icon={<LogoutOutlined className="text-lg" />}
+    >
+      Logout
+    </Button>
+  </div>
+);
+
 export function UserInfo({ name, avatar }: Props) {
   return (
-    <div className="flex gap-1 items-center justify-center">
+    <Popover
+      content={popoverContent}
+      placement="bottomLeft"
+      title={name}
+      className="flex gap-1 items-center justify-center cursor-pointer"
+    >
       <img
         src={avatar}
         alt={name}
         referrerPolicy="no-referrer"
-        className="w-8 rounded-full"
+        className="w-10 rounded-full"
       />
-      <span className="block text-sm sm:text-base text-zinc-100 whitespace-nowrap overflow-hidden text-ellipsis">{name}</span>
-    </div>
+
+      <CaretDownOutlined className="text-zinc-300" />
+    </Popover>
   );
 }
