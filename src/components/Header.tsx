d1262423b36823fc688028f04e530e49dbbb489e
Francis Verissimo <francissv97@gmail.com>
2022-10-03 21:07:26 -0400

    show avatar user

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,17 @@
+import { useAuth } from "../hooks/useAuth";
 import { Logo } from "./Logo";
+import { UserInfo } from "./UserInfo";
 
 export function Header() {
+  const { user } = useAuth();
+
   return (
-    <div className="bg-zinc-600 flex items-center justify-center pt-4 pb-16">
-      <Logo />
+    <div className="bg-zinc-600 pt-4 pb-16">
+      <div className="flex items-center justify-between gap-6 max-w-4xl px-4 mx-auto">
+        <Logo  />
+
+        {user && <UserInfo name={user.name} avatar={user.avatar} />}
+      </div>
     </div>
   );
 }
