f851709eb75238af12315b750d92c02d09065ae4
Francis Verissimo <francissv97@gmail.com>
2022-10-18 23:21:16 -0400

    create SimpleHeader component

---
+++ src/components/SimpleHeader.tsx
@@ -0,0 +1,24 @@
+import { ArrowUUpLeft } from "phosphor-react";
+import { useNavigate } from "react-router-dom";
+
+export function SimpleHeader() {
+  const navigate = useNavigate();
+
+  return (
+    <div className=" bg-zinc-600">
+      <div className="flex items-center justify-between gap-4 max-w-4xl mx-auto p-4">
+        <div
+          onClick={() => navigate(-1)}
+          className="group p-2 pl-0 cursor-pointer"
+        >
+          <ArrowUUpLeft
+            size={32}
+            className="group text-zinc-400 group-hover:text-zinc-200"
+          />
+        </div>
+        <strong className="text-xl text-zinc-300 font-normal">Perfil</strong>
+        <div></div>
+      </div>
+    </div>
+  );
+}
