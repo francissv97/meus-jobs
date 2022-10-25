23be4e91a06ea15641334cf5e850757afe025f4a
Francis Verissimo <francissv97@gmail.com>
2022-10-24 23:04:07 -0400

    refact a litle...

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,53 +1,56 @@
+import { useState } from "react";
 import { useAuth } from "../hooks/useAuth";
 import { UserInfo } from "./UserInfo";
 import { Logo } from "./Logo";
-import { Plus } from "phosphor-react";
-import { useState } from "react";
 import { AddNewJobModal } from "./AddNewJobModal";
+import { Job } from "../types";
+import { calculateJobsNumbers } from "../utils";
+import { Plus } from "phosphor-react";
 
 interface Props {
-  totalJobs: number;
-  inProgressJobs: number;
-  finalizedsJobs: number;
+  jobs: Job[] | undefined;
 }
 
-export function Header({ totalJobs, inProgressJobs, finalizedsJobs }: Props) {
+export function Header({ jobs }: Props) {
   const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const numbers = jobs && calculateJobsNumbers(jobs);
+
   const { user } = useAuth();
 
   const handleModalClose = () => setIsModalOpen(false);
 
   return (
-    <div className="bg-zinc-600 pt-4 pb-16 px-4">
+    <div className="bg-zinc-600 pt-4 pb-16">
       <div className="flex flex-col justify-between gap-6 max-w-4xl mx-auto">
-        <div className="flex w-full items-center justify-between">
+        <div className="flex w-full items-center justify-between px-4">
           <Logo />
 
           {user && <UserInfo name={user.name} avatar={user.avatar} />}
         </div>
 
-        <div className="flex justify-between gap-x-2 gap-y-4 flex-wrap">
+        <div className="flex justify-between gap-x-2 gap-y-4 flex-wrap px-4">
           <div className="flex gap-2">
             <p className="flex flex-col text-zinc-300">
               <strong className="font-normal text-2xl text-zinc-100">
-                {totalJobs}
+                {jobs?.length}
               </strong>
               Total
             </p>
 
-            {/* <p className="flex flex-col text-zinc-300">
+            <p className="flex flex-col text-zinc-300">
               <strong className="font-normal text-2xl text-zinc-100">
-                {inProgressJobs}
+                {numbers?.inProgress}
               </strong>
               Em andamento
-            </p> */}
+            </p>
 
-            {/* <p className="flex flex-col text-zinc-300">
+            <p className="flex flex-col text-zinc-300">
               <strong className="font-normal text-2xl text-zinc-100">
-                {finalizedsJobs}
+                {numbers?.closeds}
               </strong>
-              Finalizados
-            </p> */}
+              Encerrados
+            </p>
           </div>
 
           <button
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -14,7 +14,6 @@ import { Header } from "../components/Header";
 import { JobCard } from "../components/JobCard";
 import { Footer } from "../components/Footer";
 import { CircleNotch, SmileyWink } from "phosphor-react";
-import { calculateJobDeadline } from "../utils";
 
 export function Dashboard() {
   const { user } = useAuth();
@@ -22,9 +21,6 @@ export function Dashboard() {
 
   const [jobs, setJobs] = useState<Job[]>();
   const [profileData, setProfileData] = useState<ProfileType>();
-  const [jobsTotal, setJobsTotal] = useState(0);
-  const [jobsInProgress, setJobsInProgress] = useState(0);
-  const [jobsFinalizeds, setJobsFinalizeds] = useState(0);
 
   async function removeJob(jobId: string, allJobs: Job[]) {
     if (user?.email) {
@@ -37,28 +33,6 @@ export function Dashboard() {
       }).catch((error) => console.error(error));
     }
   }
-
-  async function calcNumbers(jobs: Job[]) {
-    if (jobs) {
-      jobs.forEach((j) => {
-        const deadline = calculateJobDeadline(
-          j.dailyHours,
-          j.totalHours,
-          j.createdAt
-        );
-
-        if (deadline > 0) {
-          const counter = jobsInProgress + 1;
-          setJobsInProgress(counter);
-        } else {
-          const counter = jobsFinalizeds + 1;
-          setJobsInProgress(counter);
-        }
-      });
-    }
-  }
-
-  // jobs && calcNumbers(jobs);
 
   useEffect(() => {
     if (user?.email) {
@@ -94,8 +68,6 @@ export function Dashboard() {
         const data = doc.data() as UserFirestoreDocData;
 
         setJobs(data.jobs);
-
-        setJobsTotal(data.jobs.length);
       });
 
       return unsub;
@@ -105,9 +77,7 @@ export function Dashboard() {
   return (
     <div className="flex flex-col bg-gradient-to-t from-zinc-400 via-zinc-300 to-zinc-200 min-h-screen">
       <Header
-        totalJobs={jobsTotal}
-        inProgressJobs={jobsInProgress}
-        finalizedsJobs={jobsFinalizeds}
+        jobs={jobs}
       />
 
       <div className="flex flex-col gap-4 mt-4 max-w-4xl mx-auto -translate-y-12 w-full px-4">
