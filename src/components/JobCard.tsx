e8fbb60948f2c37c24c5f98c511ecc113bd7001a
francissverissimo <francissv97@gmail.com>
2023-05-05 23:09:08 -0400

    style: improve css

--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -100,7 +100,7 @@ export function JobCard({ job, profileData }: JobCardProps) {
 
   return (
     <div
-      className={`grid grid-cols-3 bg-zinc-100 gap-4 px-4 py-6 rounded shadow-xl ${
+      className={`flex flex-col bg-zinc-100 gap-2 p-4 rounded shadow-xl ${
         job.markedAsDone
           ? "border-l-4 border-green-600"
           : jobInProgress
@@ -108,16 +108,54 @@ export function JobCard({ job, profileData }: JobCardProps) {
           : "border-l-4 border-red-700"
       }`}
     >
-      <div className="flex col-span-2 flex-col gap-2">
-        <div id="JobName" className="flex items-center">
-          <span className="text-zinc-700 text-xl font-medium overflow-hidden text-ellipsis">
+      <div className="flex flex-col gap-2">
+        <div className="flex flex-row-reverse flex-wrap gap-x-2">
+          <div className="flex self-end items-center">
+            <div
+              className={`${
+                job.markedAsDone
+                  ? "bg-green-300/20 text-green-700"
+                  : jobInProgress
+                  ? "bg-sky-300/20 text-sky-700"
+                  : "bg-red-300/20 text-red-700"
+              } px-2 py-1 text-sm text-center rounded h-fit w-fit`}
+            >
+              <span className="whitespace-nowrap font-medium">
+                {job.markedAsDone
+                  ? "ENCERRADO"
+                  : jobInProgress
+                  ? "EM ANDAMENTO"
+                  : "PRAZO MAX. ATINGIDO"}
+              </span>
+            </div>
+
+            <button
+              onClick={confirmMarkAsDoneJob}
+              disabled={job.markedAsDone}
+              className={`${
+                job.markedAsDone
+                  ? "text-green-700"
+                  : "text-zinc-500 hover:bg-green-50 hover:text-green-700 hover:shadow-lg"
+              } py-1 px-2 rounded-full duration-300`}
+            >
+              {job.markedAsDone ? (
+                <Checks size={32} />
+              ) : (
+                <CheckCircle size={32} />
+              )}
+            </button>
+          </div>
+
+          <span className="text-zinc-700 text-lg md:text-xl font-medium mt-2 mr-auto">
             {job.title}
           </span>
         </div>
+      </div>
 
+      <div className="flex flex-col sm:flex-row gap-4">
         <div className="flex flex-col">
           <span className="text-sm text-zinc-500">Prazo</span>
-          <span className="text-zinc-700">
+          <span className="text-zinc-700 text-base">
             {job.markedAsDone
               ? "Encerrado"
               : jobInProgress
@@ -126,9 +164,9 @@ export function JobCard({ job, profileData }: JobCardProps) {
           </span>
         </div>
 
-        <div id="JobValor" className="flex flex-col justify-center">
-          <span className="text-sm text-zinc-500">Valor</span>
-          <span className="text-zinc-700 min-h-[20px] leading-none">
+        <div className="flex flex-col justify-center">
+          <span className="text-sm text-zinc-600">Valor</span>
+          <span className="text-zinc-700 text-base min-h-[20px]">
             {jobValue &&
               jobValue.toLocaleString("pt-BR", {
                 style: "currency",
@@ -136,61 +174,25 @@ export function JobCard({ job, profileData }: JobCardProps) {
               })}
           </span>
         </div>
-      </div>
 
-      <div className="flex flex-col justify-between">
-        <div className="flex self-end items-center">
-          <div
-            className={`${
-              job.markedAsDone
-                ? "bg-green-300/20 text-green-700"
-                : jobInProgress
-                ? "bg-sky-300/20 text-sky-700"
-                : "bg-red-300/20 text-red-700"
-            } px-2 py-1 text-sm text-center rounded h-fit w-fit`}
-          >
-            <span className="whitespace-nowrap font-medium">
-              {job.markedAsDone
-                ? "ENCERRADO"
-                : jobInProgress
-                ? "EM ANDAMENTO"
-                : "PRAZO MAX. ATINGIDO"}
-            </span>
-          </div>
-
-          <button
-            onClick={confirmMarkAsDoneJob}
-            disabled={job.markedAsDone}
-            className={`${
-              job.markedAsDone
-                ? "text-green-700"
-                : "text-zinc-500 hover:bg-green-50 hover:text-green-700 hover:shadow-lg"
-            } py-1 px-1 ml-1 rounded-full duration-300`}
-          >
-            {job.markedAsDone ? (
-              <Checks size={32} />
-            ) : (
-              <CheckCircle size={32} />
+        <div className="ml-auto">
+          <div className="flex justify-end gap-2 md:gap-4">
+            {!job.markedAsDone && (
+              <button
+                onClick={() => setIsEditJobModalOpen(true)}
+                className="text-zinc-500 py-1 px-2 rounded-full duration-300 hover:bg-zinc-50 hover:shadow-lg"
+              >
+                <PencilSimpleLine size={32} />
+              </button>
             )}
-          </button>
-        </div>
 
-        <div className="flex justify-end gap-2 md:gap-4">
-          {!job.markedAsDone && (
             <button
-              onClick={() => setIsEditJobModalOpen(true)}
-              className="text-zinc-500 py-1 px-2 rounded-full duration-300 hover:bg-zinc-50 hover:shadow-lg"
+              onClick={confirmDeleteJob}
+              className="text-red-700 py-1 px-2 rounded-full duration-300 hover:bg-red-100 hover:shadow-lg"
             >
-              <PencilSimpleLine size={32} />
+              <TrashSimple size={32} />
             </button>
-          )}
-
-          <button
-            onClick={confirmDeleteJob}
-            className="text-red-700 py-1 px-2 rounded-full duration-300 hover:bg-red-100 hover:shadow-lg"
-          >
-            <TrashSimple size={32} />
-          </button>
+          </div>
         </div>
       </div>
 
